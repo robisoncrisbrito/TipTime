a550d7cb1a654ed9c02e7968dab954b01645696a
robisoncrisbrito <robison.brito@gmail.com>
2023-09-09 10:50:09 -0300

    Funcionalidades extras

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,31 +1,33 @@
 package com.example.tiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate( layoutInflater )
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
-        setContentView( binding.root)
+        setContentView(binding.root)
 
     }
 
     fun calculateTip(view: View) {
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
 
-        val selectedId = binding.tipOption.checkedRadioButtonId
+        val cost = stringInTextField.toDoubleOrNull() ?: return
 
-        val tipPercentage = when (selectedId ) {
+        val tipPercentage = when (binding.tipOption.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
@@ -35,12 +37,25 @@ class MainActivity : AppCompatActivity() {
 
         val roundUp = binding.roundUpSwitch.isChecked
 
-        if ( roundUp ) {
-            tip = kotlin.math.ceil( tip )
+        if (roundUp) {
+            tip = kotlin.math.ceil(tip)
         }
 
-        val formattedTip = NumberFormat.getCurrencyInstance().format( tip )
-        binding.tipResult.text = getString( R.string.tip_amount_20_00, formattedTip )
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount_20_00, formattedTip)
 
+        binding.costOfService.setOnKeyListener(View.OnKeyListener { view, i, _ -> handleKeyEvent( view, i ) } )
+
+    }
+
+    private fun handleKeyEvent ( view : View, keyCode : Int) : Boolean {
+
+        if ( keyCode == KeyEvent.KEYCODE_ENTER ) {
+            val inputMethodManager = getSystemService( Context.INPUT_METHOD_SERVICE ) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow( view.windowToken, 0 )
+            return true
+        }
+
+        return false
     }
 }
